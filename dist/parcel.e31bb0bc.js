ebb5d09b633318555968bd38e6dcea94cd125778
rafa <rafaelsistem87@gmail.com>
2021-05-24 08:47:48 -0700

    setup parcel-react

--- dist/parcel.e31bb0bc.js
+++ dist/parcel.e31bb0bc.js
@@ -29733,7 +29733,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62617" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62736" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
