b41631dc6b330ef26b4f151ad0149725a0818043
rafa <rafaelsistem87@gmail.com>
2021-05-25 11:38:40 -0700

    .json

--- dist/parcel_rafaga.e31bb0bc.js
+++ dist/parcel_rafaga.e31bb0bc.js
@@ -30741,7 +30741,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64323" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64717" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
